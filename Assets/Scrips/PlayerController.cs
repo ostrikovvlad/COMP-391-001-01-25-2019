a3634b2784b6e4505c3249b0799b5c665b884cd5
Vladislav <ostrikovvlad20@gmail.com>
2019-02-08 19:52:50 -0500

    Movement and boundaries

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 10.0f;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private Rigidbody2D rBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -18,13 +28,20 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        
 
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
 
         // Debug.Log("H: " + horiz + " , V: " + vert);
+        Vector2 newVelocity = new Vector2(horiz, vert);
 
-        rBody.velocity = new
+        rBody.velocity = newVelocity * speed;
+
+        // Restrict the player from leaving the play area
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, minX, maxX),
+            Mathf.Clamp(rBody.position.y, minY, maxY));
+        
     }
 }
