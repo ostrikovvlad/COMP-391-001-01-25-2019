fd5d28d214c275d76e4c0d43510df86d4b6951eb
Vladislav <ostrikovvlad20@gmail.com>
2019-02-08 21:38:24 -0500

    Laser and boundaries

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -11,7 +11,12 @@ public class PlayerController : MonoBehaviour
     public float minY;
     public float maxY;
 
+    public GameObject laser;
+    public Transform laserSpawn;
+    public float fireRate = 0.5f;
+
     private Rigidbody2D rBody;
+    private float counter = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +28,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        counter += Time.deltaTime;
+        if (Input.GetButton("Fire1") && counter > fireRate)
+        {
+            //Create my laser object
+            Instantiate(laser, laserSpawn.position, laser.transform.rotation);
+            counter = 0.0f;
+        }
     }
 
     private void FixedUpdate()
